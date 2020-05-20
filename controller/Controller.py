1098e1e0c588b77c6c58f5f4476d41678373c486
Liptonski <mcieslik@student.agh.edu.pl>
2020-05-20 02:07:45 +0200

    zmiany w kontrolerze i mainie

--- controller/Controller.py
+++ controller/Controller.py
@@ -2,6 +2,7 @@ from road.road_provider import road_provider
 from model.model import Model
 from road.road import Road
 from typing import Tuple, List
+from random import randrange
 
 class Controller:
 
@@ -17,12 +18,14 @@ class Controller:
 
     def get_result(self):
         #To be done
-        roads = self.generate_roads();
+        roads = self.generate_roads()
         #speeds dont fit model, they should be list of tuples
         #speeds = [(60, 3), (50, 2), (40, 1)]
-        speeds =[]
+        #2nd parameter is time that speed was measured in hours, to be fixed
+        speeds = []
         for road in roads:
-            speeds.append([fragment.speed for fragment in road.fragments])
+            # get rid of random
+            speeds.append([(fragment.speed, randrange(1, 5)) for fragment in road.fragments])
 
         #width from roads?
         sd_paved_width = 0.5
@@ -31,5 +34,9 @@ class Controller:
         bendiness = 199.3
         density_of_intersections = 4.3
         model = Model()
-        return model.get_average_daily_traffic(speeds, sd_paved_width, paved_width, extra_lateral_clearance,
+        average_traffic = model.get_average_daily_traffic(speeds, sd_paved_width, paved_width, extra_lateral_clearance,
                                                bendiness, density_of_intersections)
+        hourly_traffic = model.get_traffic_for_time_period(roads[0].fragments[0].speed, sd_paved_width, paved_width,
+                                                           extra_lateral_clearance,bendiness,density_of_intersections)
+
+        return (average_traffic, hourly_traffic)
