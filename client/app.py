a937252faaca4e0d80cec8e367967ce0d8608ed6
psm8 <43780167+psm8@users.noreply.github.com>
2020-06-16 20:11:28 +0200

    Design-Studio-Trafic-Flow-Ninja-31 Opracować sposób wyznaczania konkretnego odcinka drogi

--- client/app.py
+++ client/app.py
@@ -7,6 +7,7 @@ import sys
 
 
 def main(pos: Tuple[float, float],
+         lookup_range: float,
          list_roads: bool = False,
          road: int = 0,
          road_provider_name: str = "default_road_provider"):
@@ -23,7 +24,7 @@ def main(pos: Tuple[float, float],
 
     print("cars per hour: ",
         controller.get_result(
-            roads[max(0, min(road, len(roads) - 1))]
+            roads[max(0, min(road, len(roads) - 1))], lookup_range/2, pos
         )
     )
 
@@ -32,15 +33,17 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser("Traffic Flow Ninja")
     parser.add_argument("latitude")
     parser.add_argument("longitude")
+    parser.add_argument("--length", default=1000, help="length of road taken into account (real road length will be slightly bigger)")
     parser.add_argument("--list-roads", action='store_true', help="(toggle) List all roads at a certain position")
     parser.add_argument("--road", default=0, help="which road to chose (only has effect if there are multiple roads)")
     parser.add_argument("--road-provider", default="default_road_provider",
                         help="alternate module providing road geometry")
     options = parser.parse_args()
 
-    pos = (float(options.latitude),float(options.longitude))
+    pos = (float(options.latitude), float(options.longitude))
     # pos = (49.66006, 19.26671)  # FOr Testing purposes
+    lookup_range = int(options.length)
     list_roads = options.list_roads is not None
     road = int(options.road)
 
-    main(pos=pos, list_roads=list_roads, road=road, road_provider_name=options.road_provider)
+    main(pos=pos, lookup_range=lookup_range, list_roads=list_roads, road=road, road_provider_name=options.road_provider)
