c08b6a4dde91906fa7f7cda246261ec054c3a063
psm8 <43780167+psm8@users.noreply.github.com>
2020-06-16 20:38:34 +0200

    change model to cars par day

--- model/model.py
+++ model/model.py
@@ -47,29 +47,9 @@ class Model:
     def _road_to_intersection_density(road: Road):
         return road.intersections / (road.length() / 1000)
 
-    # TODO this is an api change to aid with refactor- this should probably be executed with mor forethought
-    def get_average_daily_traffic(self, road: Road, percentile_speed=0.5):
-        speed_data = Model._road_to_speed_data(road)
-        width = Model._road_to_width(road)
-        bendiness = Model._road_to_bendiness(road)
-
-        density_of_intersections = Model._road_to_intersection_density(road)  # and this is..too powerfull?
-        print("density", density_of_intersections)
-        print("road:", road, road.length())
-        extra_lateral_clearance = Model._road_to_extra_lateral_clearance(road)
-
-        return self._get_average_daily_traffic(
-            speed_in_hours=speed_data,
-            sd_paved_width=self.SD_PAVED_WIDTH,
-            paved_width=width,
-            extra_lateral_clearance=extra_lateral_clearance,
-            bendiness=bendiness,
-            density_of_intersections=density_of_intersections,
-            percentile_speed=percentile_speed
-        )
-
     # TODO this is an api change to aid with refactor- this should probably be executed with mor fore thought
-    def get_traffic_for_time_period(self, road: Road, duration_hours=1, percentile_speed=0.5):
+    def get_traffic_per_day(self, road: Road, percentile_speed=0.5):
+        duration_hours = 24
         speed = Model._road_to_speed(road)
         width = Model._road_to_width(road)
         bendiness = Model._road_to_bendiness(road)
@@ -90,23 +70,6 @@ class Model:
             percentile_speed=percentile_speed
         )
 
-    # allows divide data from day into multiple periods
-    def _get_average_daily_traffic(self, speed_in_hours: [(float, float)], sd_paved_width: float,
-                                   paved_width: float,
-                                   extra_lateral_clearance: float, bendiness: float,
-                                   density_of_intersections: float,
-                                   percentile_speed) -> float:
-        measurement_duration = sum(x[1] for x in speed_in_hours)
-
-        daily_traffic = sum([self._get_traffic_for_time_period(speed_in_hours[i][0], sd_paved_width, paved_width,
-                                                               extra_lateral_clearance, bendiness,
-                                                               density_of_intersections,
-                                                               duration_hours=speed_in_hours[i][1],
-                                                               percentile_speed=percentile_speed)
-                             for i in range(len(speed_in_hours))]) / (measurement_duration / 24)
-
-        return daily_traffic
-
     # as default uses average speed (percentile = 0.5)
     def _get_traffic_for_time_period(self, speed: float, sd_paved_width: float, paved_width: float,
                                      extra_lateral_clearance: float, bendiness: float,
@@ -122,23 +85,10 @@ class Model:
                                                           segment_characteristic) + self.SD_PAVED_WIDTH_COEFF *
                                                       math.log(sd_paved_width) + (1 / self.THETA) * math.log(
                     percentile_speed)))
-                                  / self.AVERAGE_DAILY_TRAFFIC_COEFF)) * (duration_hours / 24)
+                                  / self.AVERAGE_DAILY_TRAFFIC_COEFF)) * (duration_hours/24)
 
         return daily_traffic
 
 
 def model_provider(module: str) -> Model:
     return Model()
-# # to be deleted later
-# def test_traffic_model():
-#     speeds = [(60, 3), (50, 2), (40, 1)]
-#     sd_paved_width = 0.5
-#     paved_width = 4.4
-#     extra_lateral_clearance = 1.4
-#     bendiness = 199.3
-#     density_of_intersections = 4.3
-#     model = Model()
-#     return model.get_average_daily_traffic(speeds, sd_paved_width, paved_width, extra_lateral_clearance,
-#                                            bendiness, density_of_intersections)
-#
-#     print(test_traffic_model())
